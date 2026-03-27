f40274c9b9a68f73fcd73456027a4f95a48018f5
Srihari Ponakala <85178734+S4srihari@users.noreply.github.com>
2026-03-27 11:18:03 -0400

    Time: 254 ms (37.88%) | Memory: 26.8 MB (23.83%) - LeetSync

--- 200-number-of-islands/number-of-islands.py
+++ 200-number-of-islands/number-of-islands.py
@@ -1,21 +1,27 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        m,n = len(grid), len(grid[0])
         islands = 0
-        vis = [[False]*n for _ in range(m)]
+        rows = len(grid)
+        cols = len(grid[0])
 
-        def dfs(i,j):
-            directions = [(1,0),(-1,0),(0,1),(0,-1)]
-            for di,dj in directions:
-                if 0<= i+di < m and 0<= j +dj < n and not vis[i+di][j+dj] and grid[i+di][j+dj] == "1":
-                    vis[i+di][j+dj] = True
-                    dfs(i+di,j+dj)
+        vis = set()
 
+        def expand(r,c):
+            vis.add((r,c))
+            dirs = [(0,1),(0,-1),(1,0),(-1,0)]
+            que = deque()
+            que.append((r,c))
+            while que:
+                row,col = que.popleft()
+                for dr,dc in dirs:
+                    if 0 <= row+dr < rows and 0 <= col+dc < cols:
+                        if (row+dr, col+dc) not in vis and grid[row+dr][col+dc] == '1':
+                            vis.add((row+dr, col+dc))
+                            que.append((row+dr, col+dc)) 
 
-        for i in range(m):
-            for j in range(n):
-                if grid[i][j] == "1" and not vis[i][j]:
-                    vis[i][j] = True
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1' and (i,j) not in vis:
                     islands += 1
-                    dfs(i,j)
+                    expand(i,j)
         return islands
